8e2a4aaef7924ba3e37a03e4e7d0ed187c3297df
Nicholas Halloran <nmhalloran@gmail.com>
2018-08-05 23:19:14 -0700

    Add first https req

---
+++ Exercise Files/Ch06/06_01/start/request.js
@@ -0,0 +1,43 @@
+const https = require("https");
+const fs = require("fs");
+
+let options = {
+  hostname: "en.wikipedia.org",
+  port: 443,
+  path: "/wiki/George_Washington",
+  method: "GET"
+};
+
+let req = https.request(options, function(res) {
+  let responseBody = "";
+
+  console.log("Response from server started.");
+
+  console.log(`Server Status: ${res.statusCode} `);
+  console.log("Resonse Headers: %j", res.headers);
+
+  res.setEncoding("UTF-8");
+
+  res.once("data", function(chunk) {
+    console.log(chunk);
+  });
+
+  res.on("data", function(chunk) {
+    console.log(`--chnunk--${chunk.length}`);
+    responseBody += chunk;
+  });
+
+  res.on("end", function() {
+    fs.writeFile("george-washington.html", responseBody, function(err) {
+      if (err) {
+        throw err;
+      }
+      console.log("File Downloaded");
+    });
+  });
+  res.on("error", function(err) {
+    console.log(`problem with request: ${err.message} `);
+  });
+});
+
+req.end();
