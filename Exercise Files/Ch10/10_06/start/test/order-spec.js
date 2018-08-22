f763f99127356572f3c41f8a17faeb60ab4b978a
Nicholas Halloran <nmhalloran@gmail.com>
2018-08-22 14:36:31 -0700

    check istanbul test converage

--- Exercise Files/Ch10/10_06/start/test/order-spec.js
+++ Exercise Files/Ch10/10_06/start/test/order-spec.js
@@ -26,6 +26,11 @@ describe("Ordering Items", function() {
     order.__set__("warehouse", this.warehouse);
   });
 
+  it("Logs 'item not found'", function() {
+    order.orderItem("ZZZ", 10);
+    expect(this.console.log.calledWith("Item - ZZZ not found")).to.equal(true);
+  });
+
   it("order an item when there are enough in stock", function(done) {
     var _this = this;
 
