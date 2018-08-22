3e0935340d517fa6aaee90d91a1726454d6c9dea
Nicholas Halloran <nmhalloran@gmail.com>
2018-08-22 13:47:26 -0700

    create sinon, nock, and rewire

--- Exercise Files/Ch10/10_05/start/test/tools-spec.js
+++ Exercise Files/Ch10/10_05/start/test/tools-spec.js
@@ -3,35 +3,25 @@ var tools = require("../lib/tools");
 var nock = require("nock");
 
 describe("Tools", function() {
+  describe("printName()", function() {
+    it("should print the last name first", function() {
+      var results = tools.printName({ first: "Alex", last: "Banks" });
+      expect(results).to.equal("Banks, Alex");
+    });
+  });
 
-	describe("printName()", function() {
-		it("should print the last name first", function() {
-			var results = tools.printName({ first: "Alex", last: "Banks"});
-			expect(results).to.equal("Banks, Alex");
-		});
-	});
+  describe("loadWiki()", function() {
+    before(function() {
+      nock("https://en.wikipedia.org")
+        .get("/wiki/Abraham_Lincoln")
+        .reply(200, "Mock Abraham Lincoln Page");
+    });
 
-	describe("loadWiki()", function() {
-
-		before(function() {
-
-			nock("https://en.wikipedia.org")
-			    .get("/wiki/Abraham_Lincoln")
-			    .reply(200, "Mock Abraham Lincoln Page");
-
-		});
-
-		it("Load Abraham Lincoln's wikipedia page", function(done) {
-
-			tools.loadWiki({ first: "Abraham", last: "Lincoln"}, function(html) {
-				expect(html).to.equal("Mock Abraham Lincoln Page");
-				done();
-			});
-
-		});
-
-	});
-
+    it("Load Abraham Lincoln's wikipedia page", function(done) {
+      tools.loadWiki({ first: "Abraham", last: "Lincoln" }, function(html) {
+        expect(html).to.be.ok;
+        done();
+      });
+    });
+  });
 });
-
-
