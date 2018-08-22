4c4992283fbea374e01e870ba4bc5d9697b6d4a0
Nicholas Halloran <nmhalloran@gmail.com>
2018-08-22 14:23:26 -0700

    sinon stubs

--- Exercise Files/Ch10/10_06/start/test/order-spec.js
+++ Exercise Files/Ch10/10_06/start/test/order-spec.js
@@ -6,35 +6,48 @@ var order = rewire("../lib/order");
 var sinon = require("sinon");
 
 describe("Ordering Items", function() {
+  beforeEach(function() {
+    this.testData = [
+      { sku: "AAA", qty: 10 },
+      { sku: "BBB", qty: 0 },
+      { sku: "CCC", qty: 3 }
+    ];
 
-	beforeEach(function() {
+    this.console = {
+      log: sinon.spy()
+    };
 
-		this.testData = [
-			{sku: "AAA", qty: 10},
-			{sku: "BBB", qty: 0},
-			{sku: "CCC", qty: 3}
-		];
+    this.warehouse = {
+      packageAndShip: sinon.stub().yields(10987654321)
+    };
 
-		this.console = {
-			log: sinon.spy()
-		};
+    order.__set__("inventoryData", this.testData);
+    // order.__set__("console", this.console);
+    order.__set__("warehouse", this.warehouse);
+  });
 
-		order.__set__("inventoryData", this.testData);
-		order.__set__("console", this.console);
+  it("order an item when there are enough in stock", function(done) {
+    var _this = this;
 
-	});
+    order.orderItem("CCC", 3, function() {
+      // expect(_this.console.log.callCount).to.equal(2);
 
-	it("order an item when there are enough in stock", function(done) {
+      done();
+    });
+  });
 
-		var _this = this;
+  describe("Warehouse Interaction", function() {
+    beforeEach(function() {
+      this.callback = sinon.spy();
+      order.orderItem("CCC", 2, this.callback);
+    });
 
-		order.orderItem("CCC", 3, function() {
+    it("receives a tracking number", function() {
+      expect(this.callback.calledWith(10987654321)).to.equal(true);
+    });
 
-			expect(_this.console.log.callCount).to.equal(2);
-
-			done();
-		});
-
-	});
-
+    it("calls packageAndShip with the correct sku and quantity", function() {
+      expect(this.warehouse.packageAndShip.calledWith("CCC", 2)).to.equal(true);
+    });
+  });
 });
