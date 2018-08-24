fc663c93acea78b483eb474a0ef88e582250db3e
Nicholas Halloran <nmhalloran@gmail.com>
2018-08-23 23:37:28 -0700

    final portion of node

--- Exercise Files/Ch11/11_04/start/app-client.js
+++ Exercise Files/Ch11/11_04/start/app-client.js
@@ -1,13 +1,17 @@
 var $ = require("jquery");
 var printTerms = require("./lib/printTerms");
 
-$(document).ready(function () {
+$(document).ready(function() {
+  $.getJSON("/dictionary-api", printTerms);
+  $("form").submit(e => {
+    e.preventDefault();
+    $.post(
+      "/dictionary-api",
+      { term: $("#term").val(), defined: $("#defined").val() },
+      printTerms
+    );
+    document.forms[0].reset();
+  });
 
-    $.getJSON('/dictionary-api', printTerms);
-    $('form').submit(e => {
-        e.preventDefault();
-        $.post('/dictionary-api', {term: $('#term').val(), defined: $('#defined').val()}, printTerms);
-        document.forms[0].reset();
-    });
-
+  alert("Hello's World");
 });
